4202737e6a00f9f506c858e2351f96f7fc66cff6
say-hai <mhlqian@qq.com>
2020-04-25 10:49:34 +0800

    (dev) project work finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,32 @@ import {ReactiveFormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
 import {TopBarComponent} from './top-bar/top-bar.component';
 import {ProductListComponent} from './product-list/product-list.component';
+import {ProductAlertsComponent} from './product-alerts/product-alerts.component';
+import {ProductDetailComponent} from './product-detail/product-detail.component';
+import { CartComponent } from './cart/cart.component';
+import {HttpClientModule} from '@angular/common/http';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
+    HttpClientModule,
     RouterModule.forRoot([
       {path: '', component: ProductListComponent},
+      {path: 'products/:productId', component: ProductDetailComponent},
+      { path: 'cart', component: CartComponent },
+      { path: 'shipping', component: ShippingComponent },
     ])
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
     ProductListComponent,
+    ProductAlertsComponent,
+    ProductDetailComponent,
+    CartComponent,
+    ShippingComponent,
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit} from '@angular/core';
+import {CartService} from '../cart.service';
+import {FormBuilder} from '@angular/forms';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.scss']
+})
+export class CartComponent implements OnInit {
+  items;
+  checkoutForm;
+  constructor(private cartService: CartService, private formBuilder: FormBuilder) {
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
+  }
+
+  ngOnInit() {
+    this.items = this.cartService.getItems();
+  }
+
+  onSubmit(customerData) {
+    // Process checkout data here
+    window.alert('Your order has been submitted');
+
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+  }
+
+}
